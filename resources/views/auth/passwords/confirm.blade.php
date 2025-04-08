8de8cceac31a3d704993f51adff9583ddc970cb7
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2025-04-08 20:38:14 +0200

    Update confirm.blade.php

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,54 @@
 @extends('layouts.pitcernia.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+    <div class="container h-100">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow h-100">
+                    <div class="card-header">Potwierdź hasło</div>
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+                    <div class="card-body">
+                        <p class="mb-4">
+                            Zanim przejdziesz dalej, prosimy o potwierdzenie hasła.
+                        </p>
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <form method="POST" action="{{ route('password.confirm') }}">
+                            @csrf
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="form-floating mb-3">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    name="password" required autocomplete="current-password" placeholder="Hasło">
+                                <label for="password">Hasło</label>
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback d-block mt-1" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
+                            <div class="d-flex justify-content-between align-items-center">
+                                <button type="submit" class="btn btn-outline-primary">
+                                    Potwierdź hasło
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        Zapomniałeś hasła?
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
