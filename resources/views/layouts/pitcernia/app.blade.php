4f1d39bab767f7c8f6ba02c6afa5eab6811e6bc6
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2025-04-09 07:56:17 +0200

    Update app.blade.php

--- resources/views/layouts/pitcernia/app.blade.php
+++ resources/views/layouts/pitcernia/app.blade.php
@@ -13,11 +13,6 @@
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
-    <!-- Bootstrap JS z Toastem i innymi komponentami -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
-
 </head>
 
 <body>
